6874459946cf960ff61efd3bbd19ceb0fe61ef04
iagocsimoes <iago.simoes@uvvnet.com.br>
2024-11-18 10:13:54 -0300

    api update

--- API/supabaseClient.ts
+++ API/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://gaxospsmfcnfgusefxox.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdheG9zcHNtZmNuZmd1c2VmeG94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5MzMwMjgsImV4cCI6MjA0NzUwOTAyOH0.JRTB9W92HXQY1kxpckhxqifjv9x4hHMLDEsGGOqxjwc";
+const supabaseUrl = "URL DA API";
+const supabaseKey = "CHAVE API";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
